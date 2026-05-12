e729eaf752f0ed6eb1fac718c62ca402413ddb07
Saurav Singh <sauravsingh98866@gmail.com>
2026-05-12 06:55:18 +0530

    Create Channel model for MongoDB

---
+++ backend/src/models/Channel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+  {
+    channelName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    channelBanner: {
+      type: String,
+      default: "",
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Channel = mongoose.model("Channel", channelSchema);
+
+export default Channel;
+
